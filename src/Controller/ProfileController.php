b0fda88ed0d2f276c13c45e2a5ab67a81a9ebc1b
Bilel <55830730+Bilel69500@users.noreply.github.com>
2022-06-27 11:14:26 +0200

    modify password

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\EditProfileType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -11,6 +15,39 @@ class ProfileController extends AbstractController
     #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
-        return $this->render('profile/show_profile.html.twig.twig');
+        return $this->render('profile/show_profile.html.twig');
+    }
+
+    #[Route('/profile/edit', name: 'edit_user')]
+    public function editProfile(Request $request, EntityManagerInterface $em)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_profile');
+        }
+            return $this->render('profile/edit_profile.html.twig',[
+                'form' => $form->createView(),
+            ]);
+    }
+
+
+    #[Route('/profile/edit/pass', name: 'edit_user_pass')]
+    public function editProfilePass(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasher )
+    {
+
+        if ($request->isMethod('POST')){
+
+        }
+
+        return $this->render('profile/edit_pass.html.twig',[
+
+        ]);
     }
 }
