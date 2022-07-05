501690a3992df6a31bcc9a004bf132b0e2e6d06e
Bilel <55830730+Bilel69500@users.noreply.github.com>
2022-07-05 14:21:22 +0200

    jean loup ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,12 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home.index')]
-    public function index(): Response
+    public function index(ChartBuilderInterface $chartBuilder): Response
     {
-        return $this->render('home.html.twig');
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+            'datasets' => [
+                [
+                    'label' => 'Activité de la semaine',
+                    ' backgroundColor' => 'rgb(255, 99, 132)',
+                    ' borderColor' => 'rgb(255, 99, 132)',
+                    ' data' => 'day'
+                ],
+            ],
+        ]);
+        return $this->render('home.html.twig', [
+            'chart' => $chart
+        ]);
     }
 }
