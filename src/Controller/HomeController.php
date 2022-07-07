ce3356274f6ebe75688f2cab1bea08d466238e7f
hardkos77 <hartmannjeanloup@gmail.com>
2022-07-07 15:51:00 +0200

    modifications header

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use stdClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
