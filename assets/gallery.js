56b8d63eb8acf3234bb800d7680ec6b6f20aac71
tparent <theoparent71@gmail.fom>
2022-07-07 18:17:41 +0200

    Changes by matvki

--- assets/gallery.js
+++ assets/gallery.js
@@ -6,11 +6,16 @@ buttonSubmitDate.addEventListener('click', () => {
     let year = document.getElementById("selectYear")
     let month = document.getElementById("selectMonth")
     if (year.value !== "" && month.value !== "") {
-        document.getElementById("PictureContent").innerHTML = "<i class=\"fa-solid fa-spinner-third\"></i>"
+        document.getElementById("PictureContent").innerHTML = "<i class=\"fa-solid fa-spinner fa-spin\"></i>"
         axios.get("/gallery/" + year.value + "/" + month.value)
             .then(function (response) {
                 document.getElementById("PictureContent").innerHTML = response.data.data;
                 addEventDelete(document.querySelectorAll(".btn-danger"))
+            })
+            .catch(function (e) {
+                if (e.response.status === 404) {
+                    document.getElementById("PictureContent").innerHTML = "<p class='col alert alert-danger'>" + e.response.data.data + "</p>"
+                }
             })
     } else {
         alert("Veuillez sélectionnez une période.")
